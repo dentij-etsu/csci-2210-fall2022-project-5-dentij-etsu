cefe8ad7989e7231345d74bbfa0c02530992dc59
dentij <dentij@etsu.edu>
2022-11-07 19:53:22 -0500

    Add project files.

---
+++ Project5/MainPage.xaml.cs
@@ -0,0 +1,94 @@
+﻿///////////////////////////////////////////////////////////////////////////////
+//
+// Author: Jackson Denti, dentij@etsu.edu
+// Course: CSCI-2210-001 - Data Structures
+// Assignment: Project 5
+// Description: Show a functional knowledge of tree structures
+//
+///////////////////////////////////////////////////////////////////////////////
+
+namespace Project5;
+
+public partial class MainPage : ContentPage
+{
+    TreeNode<string> currentNode;
+    Tree<string> quizTree = new Tree<string>("Lets figure out what to eat tonight. Are you a Vegetarian?");
+
+    /// <summary>
+    /// The driving code for the MainPage.xaml file
+    /// </summary>
+    public MainPage()
+	{
+        // I don't know what this is, but VS put it there so I truse it needs to be here
+		InitializeComponent();
+
+        // Creates nodes of the tree with string data
+        // The node variables are numbered with a zero based index in mind, with the root being 0
+        TreeNode<string> node1 = new TreeNode<string>("Okay, but are you like REALLY a Vegetarian?");
+        TreeNode<string> node2 = new TreeNode<string>("Okay, we are narrowing it down. Are you alergic to Shellfish?");
+        TreeNode<string> node3 = new TreeNode<string>("Okay, that restricts us a bit. Are you a Vegan?");
+        TreeNode<string> node4 = new TreeNode<string>("Okat, that opens us up. Do you like fish?");
+        TreeNode<string> node5 = new TreeNode<string>("That is okay, I am too. Do you like steak?");
+        TreeNode<string> node6 = new TreeNode<string>("Does Red Lobster sound like a nice date idea to you?");
+        TreeNode<string> node7 = new TreeNode<string>("I don't know, I guess go eat some grass");
+        TreeNode<string> node8 = new TreeNode<string>("Lets go hit up a nice salad bar");
+        TreeNode<string> node9 = new TreeNode<string>("Lets go to a nice sushi bar");
+        TreeNode<string> node10 = new TreeNode<string>("Lets just get a pizza delivered");
+        TreeNode<string> node11 = new TreeNode<string>("Yay, lets go to a steakhouse");
+        TreeNode<string> node12 = new TreeNode<string>("Cool, lets go to BBQ place.");
+        TreeNode<string> node13 = new TreeNode<string>("Better grab your lobster cracking bib, we are going to red lobster.");
+        TreeNode<string> node14 = new TreeNode<string>("Lets go munch down a nice plate of oysters Rockefeller");
+
+        // Adds all the nodes to the tree
+        quizTree.Root.SetLeftChild(node1);
+        quizTree.Root.SetRightChild(node2);
+        quizTree.Root.LeftChild.SetLeftChild(node3);
+        quizTree.Root.LeftChild.SetRightChild(node4);
+        quizTree.Root.RightChild.SetLeftChild(node5);
+        quizTree.Root.RightChild.SetRightChild(node6);
+        quizTree.Root.LeftChild.LeftChild.SetLeftChild(node7);
+        quizTree.Root.LeftChild.LeftChild.SetRightChild(node8);
+        quizTree.Root.LeftChild.RightChild.SetLeftChild(node9);
+        quizTree.Root.LeftChild.RightChild.SetRightChild(node10);
+        quizTree.Root.RightChild.LeftChild.SetLeftChild(node11);
+        quizTree.Root.RightChild.LeftChild.SetRightChild(node12);
+        quizTree.Root.RightChild.RightChild.SetLeftChild(node13);
+        quizTree.Root.RightChild.RightChild.SetRightChild(node14);
+
+        // The currentNode variable is used to manage where we are in the tree so the text can update correctly
+        currentNode = quizTree.Root;
+    }
+
+    /// <summary>
+    /// If the user clicks on the "No" button, it updates the text and current node to the correct node
+    /// </summary>
+    /// <param name="sender"></param>
+    /// <param name="e"></param>
+    private void OnNoClicked(object sender, EventArgs e)
+    {
+        if (currentNode.HasRightChild) currentNode = currentNode.RightChild;
+        label.Text = currentNode.Data;
+    }
+
+    /// <summary>
+    /// If the user clicks on the "No" button, it updates the text and current node to the correct node
+    /// </summary>
+    /// <param name="sender"></param>
+    /// <param name="e"></param>
+    private void OnYesClicked(object sender, EventArgs e)
+    {
+        if (currentNode.HasLeftChild) currentNode = currentNode.LeftChild;
+        label.Text = currentNode.Data;
+    }
+    /// <summary>
+    /// If the user clicks on the "Restart" Button, it sets the current node to the root of the tree
+    /// </summary>
+    /// <param name="sender"></param>
+    /// <param name="e"></param>
+    private void OnRestartClicked(object sender, EventArgs e)
+    {
+        currentNode = quizTree.Root;
+        label.Text = currentNode.Data;
+    }
+}
+
---
+++ Project5/MauiProgram.cs
@@ -0,0 +1,80 @@
+﻿///////////////////////////////////////////////////////////////////////////////
+//
+// Author: Jackson Denti, dentij@etsu.edu
+// Course: CSCI-2210-001 - Data Structures
+// Assignment: Project 5
+// Description: Show a functional knowledge of tree structures
+//
+///////////////////////////////////////////////////////////////////////////////
+namespace Project5;
+
+public static class MauiProgram
+{
+    /// <summary>
+    /// No clue what this is. VS generated it for GUI management purposes
+    /// </summary>
+    /// <returns> A created MauiApp object </returns>
+	public static MauiApp CreateMauiApp()
+	{
+		var builder = MauiApp.CreateBuilder();
+		builder
+			.UseMauiApp<App>()
+			.ConfigureFonts(fonts =>
+			{
+				fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+				fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+			});
+		return builder.Build();
+	}
+}
+public class TreeNode<T>
+{
+    public T Data { get; set; }
+    public List<TreeNode<T>> Children { get; private set; }
+    public TreeNode<T> LeftChild { get; private set; }
+    public TreeNode<T> RightChild { get; private set; }
+    public bool HasRightChild = false;
+    public bool HasLeftChild = false;
+
+    public TreeNode(T data)
+    {
+        Data = data;
+        //Children = new TreeNode<T>();
+    }
+
+    /// <summary>
+    /// Sets a node object in the tree. This is a binary tree implementation
+    /// and this sets the "Right" child for the node
+    /// </summary>
+    /// <param name="node"> Node object to be set </param>
+    public void SetRightChild(TreeNode<T> node)
+    {
+        HasRightChild = true;
+        RightChild = node;
+    }
+
+    /// <summary>
+    /// Sets a node object in the tree. This is a binary tree implementation
+    /// and this sets the "Left" child for the node
+    /// </summary>
+    /// <param name="node"> Node object to be set </param>
+    public void SetLeftChild(TreeNode<T> node)
+    {
+        HasLeftChild = true;
+        LeftChild = node;
+    }
+}
+
+public class Tree<T>
+{
+    public TreeNode<T> Root { get; set; }
+
+    /// <summary>
+    /// Constructor for Tree class
+    /// </summary>
+    /// <param name="data"> Starting data to be the root of the tree </param>
+    public Tree(T data)
+    {
+        this.Root = new TreeNode<T>(data);
+    }
+}
